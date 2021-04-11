ce55146a77b65d3bd03c2f4f95abb9489a098d86
Martin Bäumer <martin.baeumer@gmail.com>
2021-04-11 17:53:26 +0200

    updated exercise 16

--- 16_mnist_standardization/normal_vs_standardized.py
+++ 16_mnist_standardization/normal_vs_standardized.py
@@ -1,10 +1,13 @@
-import neural_network as nn
-#import mnist as normal
+import neural_network_ex14 as nn
+import data_handler as normal
 import standardization_example as standardized
 
+print("Original mnist")
+nn.train(normal.X_train, normal.Y_train, normal.X_validation, normal.Y_validation,
+         n_hidden_nodes = 200, epochs=2, batch_size = 60, lr = 0.1)
 
 print("Standardized mnist")
-nn.train(standardized.X_train, standardized.Y_train,
-         standardized.X_validation, standardized.Y_validation, n_hidden_nodes = 200, iterations = 60, lr = 0.1)
+nn.train(standardized.X_train, standardized.Y_train, standardized.X_validation, standardized.Y_validation,
+         n_hidden_nodes = 200, epochs=2, batch_size = 60, lr = 0.1)
 
 
