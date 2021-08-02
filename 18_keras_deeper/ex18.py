a2bd5126b7c8597f84dac6e47e0945d19cedd59f
Martin Bäumer <martin.baeumer@gmail.com>
2021-08-03 00:43:17 +0200

    added exercise 18, reworked exercise 17

--- 18_keras_deeper/ex18.py
+++ 18_keras_deeper/ex18.py
@@ -14,6 +14,7 @@ Y_validation = to_categorical(data.Y_validation)
 
 model = Sequential()
 model.add(Dense(100, activation='sigmoid'))
+model.add(Dense(30, activation='sigmoid'))
 model.add(Dense(2, activation='softmax'))
 model.compile(loss='categorical_crossentropy', optimizer=RMSprop(lr=0.001), metrics=['accuracy'])
 
@@ -21,18 +22,23 @@ model.fit(X_train, Y_train, validation_data=(X_validation, Y_validation), epochs
 
 boundary.show(model, data.X_train, data.Y_train)
 
-# results from ex 17, with 5000 epochs:
+# results from ex 17:
 # Epoch 5000/5000
 # 1/12 [=>............................] - ETA: 0s - loss: 0.4201 - accuracy: 0.8400
 # 12/12 [==============================] - 0s 3ms/step - loss: 0.3681 - accuracy: 0.8447 - val_loss: 0.3330 - val_accuracy: 0.8281
-# results from ex 17, with 15000 epochs:
-# Epoch 15000/15000
-# 1/12 [=>............................] - ETA: 0s - loss: 0.1536 - accuracy: 0.9200
-# 12/12 [==============================] - 0s 3ms/step - loss: 0.1866 - accuracy: 0.8969 - val_loss: 0.2134 - val_accuracy: 0.8982
+# results from ex 18:
+# Epoch 5000/5000
+# 1/12 [=>............................] - ETA: 0s - loss: 0.1581 - accuracy: 1.0000
+# 12/12 [==============================] - 0s 3ms/step - loss: 0.1586 - accuracy: 0.9572 - val_loss: 0.2218 - val_accuracy: 0.9123
+
 # results from ex 17, with 30000 epochs:
 # Epoch 30000/30000
 # 1/12 [=>............................] - ETA: 0s - loss: 0.1516 - accuracy: 0.9200
 # 12/12 [==============================] - 0s 3ms/step - loss: 0.1373 - accuracy: 0.9414 - val_loss: 0.1747 - val_accuracy: 0.9228
+# results from ex 18, 30000 epochs:
+# Epoch 30000/30000
+# 1/12 [=>............................] - ETA: 0s - loss: 5.2170e-04 - accuracy: 1.0000
+# 12/12 [==============================] - 0s 3ms/step - loss: 0.0125 - accuracy: 0.9966 - val_loss: 0.4554 - val_accuracy: 0.9158
 
 
 
